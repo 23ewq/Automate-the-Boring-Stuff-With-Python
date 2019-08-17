a1a3c40f0b9df4826b4d287de395d5dbab4b7171
Muzahidul Islam <34352039+Muzahidul04@users.noreply.github.com>
2019-08-16 22:31:02 -0400

    Add files via upload

---
+++ Chapter7/regexStrip.py
@@ -0,0 +1,14 @@
+"""Write a function that takes a stringand does the same thingas
+the strip() string method.  If no other arguments are passed
+other than the string to strip, then whitespace characters will be removed
+from the beginning and end of the string. Otherwise, the characters specified
+in the second argument to the function will be removed from the string.
+"""
+
+import re
+
+def regex_strip(string, remove = 'l'):
+    strip_regex = re.compile('[%s]' % remove)
+    print(strip_regex.sub('', string))
+
+regex_strip("Hello I am John")
