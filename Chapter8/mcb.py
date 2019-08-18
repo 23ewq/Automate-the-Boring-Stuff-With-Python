59e928e85570b00b262d25c3e3a2ac5cb9d97d78
Muzahidul Islam <34352039+Muzahidul04@users.noreply.github.com>
2019-08-18 19:55:57 -0400

    Update mcb.py

--- Chapter8/mcb.py
+++ Chapter8/mcb.py
@@ -1,8 +1,10 @@
 #! /usr/local/bin/python3
 # mcb.pyw - Saves and loads pieces of text to the clipboard.
 # Usage: ./mcb.py save <keyword> - Saves clipboard to keyword.
-#        ./mcb.py <keyword> - Loads keyword to clipboard.
+#        ./mcb.py.py <keyword> - Loads keyword to clipboard.
 #        ./mcb.py list - Loads all keywords to clipboard.
+#        ./mcb.py delete <keyword> - Deletes keyword from database
+#        ./mcb.py delete - Deletes everything
 
 import shelve, pyperclip, sys
 
@@ -11,12 +13,16 @@ mcbShelf = shelve.open('mcb')
 # Save clipboard content.
 if len(sys.argv) == 3 and sys.argv[1].lower() == "save":
     mcbShelf[sys.argv[2]] = pyperclip.paste()
+if len(sys.argv) == 3 and sys.argv[1].lower() == 'delete':
+    del mcbShelf[sys.argv[2]]
 elif len(sys.argv) == 2:
     # List keywords and load content
+    if sys.argv[1].lower() == 'delete':
+        for i in mcbShelf.keys():
+            del mcbShelf[i]
     if sys.argv[1].lower() == 'list':
         pyperclip.copy(str(list(mcbShelf.keys())))
     elif sys.argv[1] in mcbShelf:
         pyperclip.copy(mcbShelf[sys.argv[1]])
 
 mcbShelf.close()
-
